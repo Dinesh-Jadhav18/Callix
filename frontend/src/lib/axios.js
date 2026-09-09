894ea213c0324b55bd54ee0edcae4a340d31f827
Dinesh <dineshbjadhav2003@gmail.com>
2026-09-09 21:24:10 +0530

    Update production API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "https://callix-backend-ux9x.onrender.com/api";
+const BASE_URL = "https://callix-backend-ux9x.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
