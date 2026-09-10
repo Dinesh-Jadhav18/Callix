41bca1b0520f36308f34fdc4c59c5d8ee3c4786b
Dinesh <dineshbjadhav2003@gmail.com>
2026-09-10 12:30:07 +0530

    jwt authemtication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -92,12 +92,12 @@ export async function login(req, res) {
       expiresIn: "7d"
     });
 
-    res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true, 
-      sameSite: "none", 
-      secure: true
-    });
+res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "none",
+  secure: true
+});
 
     res.status(200).json({ success: true, user });
   } catch (error) {
